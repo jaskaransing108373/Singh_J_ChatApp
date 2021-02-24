d7058f1cc1bd2e485da9901d43fded8a60d18e36
jaskaransing108373 <jaskaransing108373@github.com>
2021-02-24 08:14:01 -0500

    added login page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,48 +1,93 @@
-import ChatMessage from "./components/TheMessageComponent.js"
+import ChatMessage from './components/TheMessageComponent.js';
 
 (() => {
-    console.log('fired');
-
-    // load socket library and make a connection 
     const socket = io();
 
-    // messenger service event handling -> incoming from the manager
-    function setUserId({sID, message}) {
-        // incoming connected event with data
-       // debugger; 
+    function setUserId({ sID }) {
+        console.log(sID);
         vm.socketID = sID;
-    }
+    };
 
     function appendMessage(message) {
         vm.messages.push(message);
-    }
+    };
+
+    function DisconnectMessage() {
+        console.log('a user has disconnected');
+    };
+
+    function AppendUser(member) {
+        vm.members.push(member);
+    };
+
     const vm = new Vue({
         data: {
             messages: [],
-            nickname: "",
-            username: "",
-            socketID: "",
-            message: ""
-        },
+            nickname: '',
+            socketID: '',
+            message: '',
+            members: []
 
-        created: function() {
-            console.log('its alive!!');
         },
 
         methods: {
             dispatchMessage() {
-             //   debugger;
-                socket.emit('chatmessage', { content: this.message, name: this.nickname || 'Anonymous'})
-                this.message = "";
+                console.log('message shot fired');
+
+                socket.emit('chat_message', {
+                    content: this.message, name: this.nickname || 'anonymous'
+                })
+                this.message = '';
+            },
+
+            addNewMember() {
+                console.log('a User joined the chat');
+
+                socket.emit('userJoined', {
+                    member: this.nickname
+                })
             }
         },
 
+        mounted: function () {
+            console.log('vue has mounted');
+        },
         components: {
             newmessage: ChatMessage
+
         }
+    }).$mount('#app');
+    socket.addEventListener('newUser', AppendUser);
+    socket.addEventListener('disconnect', DisconnectMessage);
 
-    }).$mount("#app");
+    socket.addEventListener('connected', setUserId);
+    socket.addEventListener('new_message', appendMessage);
 
-    socket.addEventListener("connected", setUserId);
-    socket.addEventListener('message', appendMessage);
+    const loginPage = document.querySelector('.loginPage'),
+        nicknameInput = document.querySelector('#nickname'),
+        loginButton = document.querySelector('.nicknameButton');
+
+    loginButton.addEventListener('click', function () {
+        if (nicknameInput.value === '') {
+
+            alert("You need to fill a Username")
+
+        }
+        else {
+            console.log('new user has joined');
+
+            loginPage.classList.add('hide');
+
+            alert('Welcome, ' + nicknameInput.value);
+        }
+    });
+
+    const UserBut = document.querySelector('.userDisplay'),
+        UserNav = document.querySelector('.memberList');
+
+    UserBut.addEventListener('click',
+        function () {
+            console.log('User nav Clicked');
+            UserNav.classList.toggle('showNav');
+        })
 })();
